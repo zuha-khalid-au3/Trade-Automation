2e71179f9d4f14df5d6bc565a83619af7a21f4be
Mohammad Zuha Khalid <zuhakhalid@gmail.com>
2024-07-20 21:08:01 +0000

    netlify chnages

--- Frontend/my-trade/src/AuthForm.js
+++ Frontend/my-trade/src/AuthForm.js
@@ -22,7 +22,7 @@ const AuthForm = () => {
 
     if (code) {
       try {
-        const response = await axios.post('/.netlify/functions/getAccessToken', {  // <-- Updated endpoint
+        const response = await axios.post('https://5000-zuhakhalida-tradeautoma-j3gpuxzd16m.ws-us115.gitpod.io/', {  // <-- Updated endpoint
           code,
           client_id: clientId,
           client_secret: process.env.REACT_APP_CLIENT_SECRET,
